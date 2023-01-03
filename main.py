4297952b1759670ac4042df9fd13562a02babac3
Одинцов Дмитрий <dmodv@vk.com>
2023-01-03 18:58:58 +0300

    logger

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class Data:
 
 class Logger_Bot:
     def __init__(self) -> None:
+        self.dump = 'dump\\'
         self.path = 'logs\\'
         day = 3
         current, dirs, files = os.walk(self.path).__next__()
@@ -56,18 +57,60 @@ class Logger_Bot:
             for i in files:
                 os.remove(self.path + i)
 
-    def __save_log(self, text):
-        path_log = self.path + 'log_' + datetime.now().strftime('%Y-%m-%d').replace(' ', '_').replace(':', '-') + ".txt"
-        arg = 'w'
-        if os.path.exists(path_log):
-            arg = 'a'
-        with open(path_log, arg) as new_log:
-            new_log.write(text + "\n")
+    def __save_log(self, text: str):
+
+        name_file = self.path + 'log_' + datetime.now().strftime('%Y-%m-%d').replace(' ', '_').replace(':', '-') + ".txt"
+
+        if os.path.exists(name_file):
+
+            with open(name_file, 'rb') as file:
+                data2 = file.read()  # прочитали
+
+            key = data2[:22] + data2[-22:]  # чтение ключа
+
+            f = Fernet(key)
+
+            dt = f.decrypt(data2[22:-22]).decode() + text
+            data3 = key[:22] + f.encrypt(dt.encode()) + key[-22:]
+
+            with open(name_file, 'wb') as new_log:
+                new_log.write(data3)
+
+        else:
+
+            key = Fernet.generate_key()  # генер ключа шифрования
+            f = Fernet(key)
+            encrypted_text = key[:22] + key[-22:]
+
+            with open(name_file, 'wb') as file:
+                file.write(key[:22] + f.encrypt(encrypted_text) + key[-22:])
+
+            self.__save_log('&#Logs&#')
 
     def log(self, name_func, e_text):
         time_e = datetime.now().strftime('%H:%M:%S').replace(' ', '_').replace(':', '-')
-        text = f"[{time_e}] {name_func}: {e_text}."
+        text = f"[{time_e}] {name_func}: {e_text}.&#"
         self.__save_log(text)
+
+    def get_log(self, name_file):
+        try:
+            with open(name_file, 'rb') as file:
+                data2 = file.read()
+            key = data2[:22] + data2[-22:]  # чтение ключа
+            f = Fernet(key)
+            fl = name_file.split('\\')[1]
+            print(fl)
+            with open(name_file, 'rb') as file:
+                data3 = file.read()[22:-22]
+
+            data3 = f.decrypt(data3).decode().replace("&#", "\n")
+
+            with open(self.dump+'\\'+fl, 'w') as file:
+                file.write(data3)
+            return True
+
+        except:
+            return False
 
 
 if not os.path.isdir('media'):
